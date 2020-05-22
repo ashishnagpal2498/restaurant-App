8c3b9b353e88e3cf8887113ec7d53b12033de5cd
unknown <ashishnagpal2498@gmail.com>
2020-05-22 16:08:55 +0530

    :star: production build

---
+++ build/precache-manifest.3913c206e054034f40d55e9be77ada9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ec32d6cc8eb7dd638fc4fa13c8a53f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "202889f2fc6ce38a1208",
+    "url": "/static/css/main.c6f8ef49.chunk.css"
+  },
+  {
+    "revision": "1b1e48501b91d84798e2",
+    "url": "/static/js/2.4e6afa47.chunk.js"
+  },
+  {
+    "revision": "7edc9d6a5411dd85ffa426b7d3cb6e20",
+    "url": "/static/js/2.4e6afa47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "202889f2fc6ce38a1208",
+    "url": "/static/js/main.8761fb36.chunk.js"
+  },
+  {
+    "revision": "05aad608fb705ee6ad3f",
+    "url": "/static/js/runtime-main.d1e356e9.js"
+  }
+]);
